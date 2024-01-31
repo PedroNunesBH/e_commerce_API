5ea6533c2a5dd2143d7dd3a6466df48e51669f57
Pedro Nunes <pedrohenriquestore99@gmail.com>
2024-01-31 11:40:11 -0300

    Criacao do model Product

--- products/models.py
+++ products/models.py
@@ -1,2 +1,11 @@
 from django.db import models
 
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    brand = models.CharField(max_length=50)
+    price = models.FloatField()
+    description = models.CharField(max_length=150)
+    model = models.CharField(max_length=50)
+    color = models.CharField(max_length=50)
+    warrant_period = models.CharField(max_length=100)
