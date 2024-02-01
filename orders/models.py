5b74b0eb67463de4bfb4bb2954e0f434429b46eb
Pedro Nunes <pedrohenriquestore99@gmail.com>
2024-02-01 12:39:03 -0300

    Criacao do model Order

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from clients.models import Client
+from products.models import Product
 
-# Create your models here.
+
+class Order(models.Model):
+    client = models.ForeignKey(Client,
+                               on_delete=models.PROTECT,
+                               related_name="client_order")  # Cliente que efetuou o pedido
+    products = models.ManyToManyField(Product, related_name='products_order')  # Produtos do pedido
+    date = models.DateTimeField(auto_now_add=True)  # Preenchimento da data e hora automático
+    total_price = models.FloatField()
+    address_to_delivery = models.TextField()
+
+    def __str__(self):
+        return self.pk  # Retorna o código da venda(id/pk)
+
+
