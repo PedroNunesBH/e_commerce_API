620349852a95bb7b896a51b645f13cc8043760b8
Pedro Nunes <pedrohenriquestore99@gmail.com>
2024-02-02 17:25:33 -0300

    Criacao de endpoints para estatisticas de metodos de pagamentos e model respectivo

--- orders/models.py
+++ orders/models.py
@@ -2,8 +2,9 @@ from django.db import models
 from clients.models import Client
 from products.models import Product
 
-PAYMENT_METHODS = (("Dinheiro", "Dinheiro"), ("Cartão de Crédito", "Cartão de Crédito"),
-                   ("PIX", "PIX"), ("Boleto", "Boleto"))
+
+class PaymentMethod(models.Model):  # Tabela de métodos de pagamentos
+    method_name = models.CharField(max_length=50)
 
 
 class Order(models.Model):
@@ -14,7 +15,7 @@ class Order(models.Model):
     date = models.DateTimeField(auto_now_add=True)  # Preenchimento da data e hora automático
     total_price = models.FloatField()
     address_to_delivery = models.TextField()
-    payment_method = models.CharField(max_length=30, choices=PAYMENT_METHODS, default="PIX")
+    payment_method = models.ForeignKey(PaymentMethod, on_delete=models.PROTECT, related_name="payment_method_order")
 
     def __str__(self):
         return self.pk  # Retorna o código da venda(id/pk)
--- orders/serializers.py
+++ orders/serializers.py
@@ -1,9 +1,16 @@
 from rest_framework import serializers
-from .models import Order
+from .models import Order, PaymentMethod
 
 
 class OrderSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Order
+        fields = "__all__"
+
+
+class PaymentMethodSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = PaymentMethod
         fields = "__all__"
