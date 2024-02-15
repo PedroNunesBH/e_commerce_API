a6adbfa5111a6857dbd1c381a69c0b2f19a84cd5
Pedro Nunes <pedrohenriquestore99@gmail.com>
2024-02-15 15:11:18 -0300

    Instalacao da biblioteca Simple JWT para autenticacao da API

--- orders/signals.py
+++ orders/signals.py
@@ -6,7 +6,8 @@ from .models import Order
 @receiver(m2m_changed, sender=Order.products.through)
 def calculate_price(sender, instance, action, **kwargs):  # Funcao responsavel por calcular o preco do pedido
     if action in ('post_add', 'post_remove', 'post_clear'):  # Chama o signal caso a acao seja uma das 3
-        total_price = sum(product.price for product in instance.products.all())  # Caclula o preco total somando os produtos do pedido
+        total_price = [product.price for product in instance.products.all()] # Caclula o preco total somando os produtos do pedido
+        total_price = sum(total_price)
         if instance.total_price != total_price:  # Verifica se o total_price é diferente do total_price do objeto
             instance.total_price = total_price  # Se for atribui o novo valor para total_price
             instance.save()  # Salva no bd
