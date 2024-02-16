842aa312a695b8fca071642777fb8cd0933ffe4d
Pedro Nunes <pedrohenriquestore99@gmail.com>
2024-02-16 15:47:36 -0300

    Criacao do endpoint para dar um refresh no acess token(TokenRefreshView)

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenVerifyView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenVerifyView, TokenRefreshView
 
 urlpatterns = [
     path('authentication/get_token/', TokenObtainPairView.as_view(), name='get_token'),
-    path('authentication/verify_token/', TokenVerifyView.as_view(), name="verify_token")
+    path('authentication/verify_token/', TokenVerifyView.as_view(), name="verify_token"),
+    path('authentication/refresh_token/', TokenRefreshView.as_view(), name='refresh_token')
 ]
